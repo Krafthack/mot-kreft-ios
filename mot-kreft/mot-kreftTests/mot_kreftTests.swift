9a1eb0850283ddeb893df233feca99b7098c9f02
Hans Magnus Inderberg <hinderberg@gmail.com>
2014-10-31 11:57:22 +0100

    Initial xcode project

---
+++ mot-kreft/mot-kreftTests/mot_kreftTests.swift
@@ -0,0 +1,36 @@
+//
+//  mot_kreftTests.swift
+//  mot-kreftTests
+//
+//  Created by Hans M. Inderberg on 10/31/14.
+//  Copyright (c) 2014 Hans M. Inderberg. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class mot_kreftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
